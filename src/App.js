e1a65a7bb0b4594c52fb9c572b20ff499093b2ee
flibbertz <yasha.vysotskiy@gmail.com>
2021-11-30 00:36:06 +0300

    finished recognition & emoji render tests

--- src/App.js
+++ src/App.js
@@ -10,104 +10,144 @@
 // add Use State DONE
 // Import emojis and finger pose import DONE
 
-// upd detect function for gesture handling
-//setup hook and emoji object
-// add emoji display to the screen
+// upd detect function for gesture handling DONE
+// setup hook and emoji object DONE
+// add emoji display to the screen DONE
 
 
-import React, {useRef, useState} from "react";
-import * as tf from '@tensorflow/tfjs'
-import * as fp from 'fingerpose'
-import victory from './victory.png'
-import thumbs_up from './thumbs_up.png'
-import * as handpose from '@tensorflow-models/handpose'
-import Webcam from 'react-webcam'
-import './App.css';
-import {drawHand} from "./utilities";
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as handpose from "@tensorflow-models/handpose";
+import Webcam from "react-webcam";
+import "./App.css";
+import { drawHand } from "./utilities";
+import testHeader from "./testHeader";
+import * as fp from "fingerpose";
+import victory from "./victory.png";
+import thumbs_up from "./thumbs_up.png";
 
 function App() {
+    const webcamRef = useRef(null);
+    const canvasRef = useRef(null);
 
-  const webcamRef = useRef(null);
-  const canvasRef = useRef(null);
+    const [emoji, setEmoji] = useState(null);
+    const images = { thumbs_up: thumbs_up, victory: victory };
 
-  const runHandpose = async () =>{
-    const net = await handpose.load()
-    console.log('Handpose model loaded.')
-    //Loop and detect hands
-    setInterval(()=>{
-      detect(net)
-    },100)
-  }
 
-  const detect = async (net)=>{
-    // Check data is available
-    if(
-        typeof webcamRef.current !== 'undefined' &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4
-    ){
-      // Get video properties
-      const video = webcamRef.current.video
-      const videoHeight = webcamRef.current.video.videoHeight
-      const videoWidth = webcamRef.current.video.videoWidth
+    const runHandpose = async () => {
+        const net = await handpose.load();
+        console.log("Handpose model loaded.");
+        //  Loop and detect hands
+        setInterval(() => {
+            detect(net);
+        }, 10);
+    };
 
-      // Set video height and width
-      webcamRef.current.video.width = videoWidth
-      webcamRef.current.video.height = videoHeight
+    const detect = async (net) => {
+        // Check data is available
+        if (
+            typeof webcamRef.current !== "undefined" &&
+            webcamRef.current !== null &&
+            webcamRef.current.video.readyState === 4
+        ) {
+            // Get Video Properties
+            const video = webcamRef.current.video;
+            const videoWidth = webcamRef.current.video.videoWidth;
+            const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set canvas height and width
-      canvasRef.current.width = videoWidth
-      canvasRef.current.height = videoHeight
+            // Set video width
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
 
-      // Make detections
-      const hand = await net.estimateHands(video)
-      console.log(hand)
+            // Set canvas height and width
+            canvasRef.current.width = videoWidth;
+            canvasRef.current.height = videoHeight;
 
-        if (hand.length > 0) {
-            const GE = new fp.GestureEstimator([
-                fp.Gestures.VictoryGesture,
-                fp.Gestures.ThumbsUpGesture,
-            ]);
-          const gesture = await GE.estimate(hand[0].landmarks,8)
-          console.log(gesture)
-      }
-      // Draw mesh
-      const ctx = canvasRef.current.getContext('2d')
-      drawHand(hand, ctx)
-    }
+            // Make Detections
+            const hand = await net.estimateHands(video);
+            // console.log(hand);
 
-  }
-  runHandpose()
-  return (
-      <div className="App">
-        <header className="App-header">
-          <Webcam ref={webcamRef}
-                  style={{
-                    position:'absolute',
-                    marginLeft:'auto',
-                    marginRight:'auto',
-                    left:0,
-                    right:0,
-                    textAlign:'center',
-                    zIndex:9,
-                    width:640,
-                    height:480
-                  }}/>
-          <canvas ref={canvasRef}
-                  style={{
-                    position:'absolute',
-                    marginLeft:'auto',
-                    marginRight:'auto',
-                    left:0,
-                    right:0,
-                    textAlign:'center',
-                    zIndex:9,
-                    width:640,
-                    height:480
-                  }}/>
-        </header>
-      </div>
-  );
+            if (hand.length > 0) {
+                const GE = new fp.GestureEstimator([
+                    fp.Gestures.VictoryGesture,
+                    fp.Gestures.ThumbsUpGesture,
+                ]);
+                const gesture = await GE.estimate(hand[0].landmarks, 4);
+                if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+                    // console.log(gesture.gestures);
+
+                    const confidence = gesture.gestures.map(
+                        (prediction) => prediction.confidence
+                    );
+                    const maxConfidence = confidence.indexOf(
+                        Math.max.apply(null, confidence)
+                    );
+                    // console.log(gesture.gestures[maxConfidence].name);
+                    setEmoji(gesture.gestures[maxConfidence].name);
+                }
+            }
+
+            // Draw mesh
+            const ctx = canvasRef.current.getContext("2d");
+            drawHand(hand, ctx);
+        }
+    };
+
+    useEffect(()=>{runHandpose()},[]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Webcam
+                    ref={webcamRef}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        left: 0,
+                        right: 0,
+                        textAlign: "center",
+                        zIndex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
+
+                <canvas
+                    ref={canvasRef}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        left: 0,
+                        right: 0,
+                        textAlign: "center",
+                        zIndex: 9,
+                        width: 640,
+                        height: 480,
+                    }}
+                />
+                if (emoji !== null) {
+                <img
+                    src={images[emoji]}
+                    style={{
+                        position: "absolute",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                        left: 400,
+                        bottom: 500,
+                        right: 0,
+                        textAlign: "center",
+                        height: 100,
+                    }}
+                />
+            }else{
+             ''
+            }
+                {/* NEW STUFF */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/testHeader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function testHeader(){
+    return(
+        <div className='testDiv'>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
