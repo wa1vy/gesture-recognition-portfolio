28974eaf2823ec035cd0887fcefd6ec13174b157
flibbertz <yasha.vysotskiy@gmail.com>
2021-11-30 14:56:52 +0300

    2 testing switches between pages

---
+++ src/components/test2Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Test2Header(){
+    return(
+        <div className='testDiv'>
+            <h1>my projects!</h1>
+        </div>
+    )
+}
---
+++ src/components/testHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function TestHeader() {
+    return(
+        <>
+            <div className='testDiv'>
+                <h1>my hard skills</h1>
+            </div>
+        </>
+
+    )
+}
+
+
