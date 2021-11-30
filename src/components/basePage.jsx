2fdd9d19ed7f23b1e53bc3590e1bb83d338e5975
flibbertz <yasha.vysotskiy@gmail.com>
2021-11-30 14:57:47 +0300

    initial page set before smth is recognised

--- src/components/basePage.jsx
+++ src/components/basePage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-export default function testHeader(){
+export default function BasePage(){
     return(
         <div className='testDiv'>
-            <h1>Hello World!</h1>
+            <h1>Basic Page</h1>
         </div>
     )
 }
