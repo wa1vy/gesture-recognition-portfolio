97f6085fcf495cd9356ced26dc9fd11e119c10b9
flibbertz <yasha.vysotskiy@gmail.com>
2021-12-01 18:14:34 +0300

    function for conditional rendering of pages

---
+++ src/conditionalRendering.jsx
@@ -0,0 +1,21 @@
+import {MyHardSkillsPage} from './components/MyHardSkillsPage'
+import MyProjectsPage from './components/MyProjectsPage'
+import BasePage from './components/basePage'
+import PalmPage from "./components/PalmPage";
+
+export const ConditionalRendering = {
+    victory: MyHardSkillsPage,
+    thumbs_up: MyProjectsPage,
+    base: BasePage,
+    rock: BasePage,
+    palm: PalmPage,
+}
+
+export function SpecificGesturePage(gestureName) {
+    const SpecificPage = ConditionalRendering[gestureName];
+    return (
+        <>
+            <SpecificPage/>
+        </>
+    );
+}
