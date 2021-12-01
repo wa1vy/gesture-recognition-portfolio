4489ed9d3efb88de8373f55612d074b61b886751
flibbertz <yasha.vysotskiy@gmail.com>
2021-12-01 15:13:58 +0300

    custom Rock Gesture recognition

---
+++ src/customGestures/rockGesture.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Finger, FingerCurl, FingerDirection, GestureDescription} from 'fingerpose';
+// Define Gesture Description
+export const RockGesture = new GestureDescription('rock')
+
+// Thumb
+RockGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0)
+RockGesture.addDirection(Finger.Thumb, FingerDirection.HorizontalLeft, 0.25);
+RockGesture.addDirection(Finger.Thumb, FingerDirection.HorizontalRight, 0.25);
+
+// Index
+RockGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0)
+RockGesture.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.35);
+
+// Pinky
+RockGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1.0)
+RockGesture.addDirection(Finger.Pinky, FingerDirection.VerticalUp, 0.35);
+
+for(let finger of [Finger.Middle, Finger.Ring]){
+    RockGesture.addCurl(finger, FingerCurl.FullCurl, .75);
+   // RockGesture.addDirection(finger, FingerDirection.VerticalDown, 0.25);
+}
