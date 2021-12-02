647dd5701ea44d7b6f17bf533ce39abfec703b33
flibbertz <yasha.vysotskiy@gmail.com>
2021-12-02 17:15:39 +0300

    contacts page

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TopBar from "../navigation/topBar";
+export default function Contacts(){
+    return(
+        <div>
+            <TopBar/>
+            <div className='testDiv'>
+                <h1>Contact me!</h1>
+            </div>
+        </div>
+
+    )
+}
